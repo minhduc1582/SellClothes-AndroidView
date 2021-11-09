7238d3d4022357f2fe383a6a8a7a47dd3d54f392
minhduc1582 <62060849+minhduc1582@users.noreply.github.com>
2021-11-09 17:01:28 +0700

    init

---
+++ app/src/main/java/com/dhruva/shopping/Api/ApiService.java
@@ -0,0 +1,25 @@
+package com.dhruva.shopping.Api;
+
+import com.dhruva.shopping.Model.User;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+
+public interface ApiService {
+    Gson gson = new GsonBuilder()
+            .setDateFormat("yyyy-MM-dd HH:mm:ss")
+            .create();
+
+    ApiService apiService = new Retrofit.Builder()
+            .baseUrl("http://118.71.142.126:8090/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(ApiService.class);
+    @GET("accounts/login")
+    Call<User> isUserOrAdmin(@Body User user);
+}
---
+++ app/src/main/java/com/dhruva/shopping/Model/User.java
@@ -0,0 +1,55 @@
+package com.dhruva.shopping.Model;
+
+import lombok.*;
+
+@Getter
+@Setter
+public class User {
+    private String name;
+    private String phone;
+    private String password;
+    private int level;
+    public User()
+    {
+
+    }
+
+    public User(String name, String phone, String password, int level) {
+        this.name = name;
+        this.phone = phone;
+        this.password = password;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
